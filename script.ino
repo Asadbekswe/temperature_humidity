9b25397c1dac57790345dbd97a4e4443bfe3cc96
Asadbekswe <asadbekmehmonjonov89@gmail.com>
2026-01-03 20:43:57 +0500

    new version code

--- script.ino
+++ script.ino
@@ -8,12 +8,28 @@
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+void startupAnimation() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" DHT22 Monitor ");
+  lcd.setCursor(0,1);
+  lcd.print(" Initializing ");
+  delay(1500);
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" Sensor Ready ");
+  delay(1000);
+}
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
 
   lcd.init();
   lcd.backlight();
+
+  startupAnimation();
 }
 
 void loop() {
@@ -23,24 +39,29 @@ void loop() {
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(" Sensor Error ");
+    lcd.setCursor(0,1);
+    lcd.print(" Check Wiring ");
     Serial.println("DHT read failed");
     return;
   }
 
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %, Temp: ");
+  Serial.print("Temp: ");
   Serial.print(t);
-  Serial.println(" C");
+  Serial.print(" C | Hum: ");
+  Serial.print(h);
+  Serial.println(" %");
 
-  lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Temp: ");
-  lcd.print(t);
-  lcd.print(" C");
+  lcd.print(t,1);
+  lcd.print((char)223);
+  lcd.print("C   ");
 
   lcd.setCursor(0,1);
-  lcd.print("Hum: ");
-  lcd.print(h);
-  lcd.print(" %");
+  lcd.print("Hum : ");
+  lcd.print(h,1);
+  lcd.print("%   ");
 }
